40802dd50fd935a40f844b7d057d5f01403a0fc8
Janosch Machowinski <Janosch.Machowinski@dfki.de>
2016-01-17 18:03:25 +0100

    enable cpp_proxy plugin

--- overrides.rb
+++ overrides.rb
@@ -2,4 +2,5 @@ Autoproj.manifest.each_autobuild_package do |pkg|
     next if !pkg.kind_of?(Autobuild::Orogen)
 
     pkg.depends_on 'tools/orogen_cpp_proxies'
-end
+    pkg.orogen_options << '--extensions=cpp_proxies'
+ end
