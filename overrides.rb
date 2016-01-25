243ae1d06b5748b50d6c95defa0c9b3d2c41fc0a
Janosch Machowinski <janosch.machowinski@dfki.de>
2016-01-25 13:28:29 +0100

    added model exporter

--- overrides.rb
+++ overrides.rb
@@ -2,5 +2,6 @@ Autoproj.manifest.each_autobuild_package do |pkg|
     next if !pkg.kind_of?(Autobuild::Orogen)
 
     pkg.depends_on 'tools/orogen_cpp_proxies'
-    pkg.orogen_options << '--extensions=cpp_proxies'
+    pkg.depends_on 'tools/orogen_model_exporter'
+    pkg.orogen_options << '--extensions=cpp_proxies,modelExport'
  end
