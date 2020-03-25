1480fe8ccbdb64be69572b20f739b205136561d6
Thomas Roehr <thomas.roehr@dfki.de>
2020-03-25 17:40:55 +0100

    Fix setting of environment variables

--- overrides.rb
+++ overrides.rb
@@ -5,6 +5,3 @@ Autoproj.manifest.each_autobuild_package do |pkg|
     pkg.depends_on 'tools/orogen_model_exporter'
     pkg.orogen_options << '--extensions=cpp_proxies,modelExport'
  end
-
-Autoproj.env_add 'OROGEN_PLUGIN_PATH', File.join(Autoproj.root_dir, 'install/tools/orogen_cpp_proxies/', 'share', 'orogen', 'plugins')
-Autoproj.env_add 'OROGEN_PLUGIN_PATH', File.join(Autoproj.root_dir, 'install/tools/orogen_model_exporter/', 'share', 'orogen', 'plugins')
